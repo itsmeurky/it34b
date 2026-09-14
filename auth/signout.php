faf8712b6accab67446077463504db688c38b60c
itsmeurky <michaelladrenojo@gmail.com>
2026-09-14 16:02:22 +0800

    added activity_logs table in the admin dashboard

--- auth/signout.php
+++ auth/signout.php
@@ -2,7 +2,7 @@
 require_once '../config/config.php';
 
     if(isset($_SESSION['user_id'])){
-        logActivity(pdo,$_SESSION['user_id'],$_SESSION['user_mail'],'logout', 'success');
+        logActivity($pdo,$_SESSION['user_id'],$_SESSION['user_mail'],'logout', 'success');
     }
 $_SESSION = [];
 
